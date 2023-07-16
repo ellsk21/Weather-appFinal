9bbbbbc9cb3751c9f34a1a9c5953a443872f0ec7
ellsk21 <87391629+ellsk21@users.noreply.github.com>
2023-07-16 14:24:47 +1200

    updated app js

--- src/app.js
+++ src/app.js
@@ -29,16 +29,21 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.conditon.icon}.png`
+  );
 }
 
 let apiKey = "65394c1aaf70b9a62t37c04bob3209ea";
-let city = "Mangawhai";
+let city = "Colorado";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
